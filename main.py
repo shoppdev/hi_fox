6aeee1e657a381de949f7b1e0b3647cb9374774d
Travis Egbert <tegbert@gmail.com>
2021-07-02 23:22:42 -0600

    basic bot created

---
+++ main.py
@@ -0,0 +1,24 @@
+import discord
+import os
+
+if __name__ == "__main__":
+
+    client = discord.Client()
+
+    @client.event
+    async def on_ready():
+        print('We have logged in as {0.user}'.format(client))
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+
+        if message.content.startswith('$hi'):
+            guy = message.author
+            await message.channel.send(f'hi {guy}')
+
+        if message.content.startswith('$fox'):
+            await message.channel.send('ｰ^｡^ｰ')
+
+    client.run('ODYwNzI5NTYxODc5Njc0ODgw.YN_e3w.wqN5xdsWGsbmpsBtBp5RbjH5jTo')
