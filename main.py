e64f2fc6c963681e54de4200dfb198322b318a64
Travis Egbert <tegbert@gmail.com>
2021-07-08 00:00:51 -0600

    added some basic functions & cheer up mode

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import discord
 import os
 
+from discord import channel
+from discord.activity import Game
+
 if __name__ == "__main__":
+
+    list_sad_words = ['sad', 'depressed', 'blue', 'down', 'heartbroken', 'mourn', 'somber', 'grief', 'hopelessness', 'unhappy']
 
     client = discord.Client()
 
+    def help_display(message):
+
+        return 'Try\n$hi - for a hello\n$fox - for a ascii fox\n$help - for list of commands'
+
     @client.event
     async def on_ready():
-        print('We have logged in as {0.user}'.format(client))
+        print('I have logged in as {0.user}'.format(client))
 
     @client.event
     async def on_message(message):
@@ -15,10 +24,27 @@ if __name__ == "__main__":
             return
 
         if message.content.startswith('$hi'):
-            guy = message.author
-            await message.channel.send(f'hi {guy}')
+            guy = message.author.display_name
+            await message.channel.send(f'~ hi {guy} ~')
 
-        if message.content.startswith('$fox'):
-            await message.channel.send('ｰ^｡^ｰ')
+        # if message.content.startswith('$fox'):
+        #     await message.channel.send('~^｡^~')
 
-    client.run('')
+        if message.content.startswith('$help'):
+            await message.channel.send(help_display(message))
+
+        if 'fox' in message.content:
+            await message.channel.send('~ Are my ears burning? ~')
+        
+        if 'plans' in message.content:
+            await message.channel.send("~ I'm free all afternoon! ~")
+
+        for word in list_sad_words:
+            if word in message.content.lower():
+                await message.channel.send(f'~ Hey, I heard your feelin {word}. ~\n~ Hope your doin ok {message.author.display_name}. ~')
+                # print(word)
+
+            
+
+
+    client.run('ODYwNzI5NTYxODc5Njc0ODgw.YN_e3w.Np38Rz3RbKavywIWWNYOx61CMAI')
